5dd7293d951f0768ba98a9b2c1655f24ba3f1428
ADITYA PATIL <adipatil011@gmail.com>
2022-09-29 23:00:53 +0530

    Client deployment successfull

--- frontend/src/components/useLogin.js
+++ frontend/src/components/useLogin.js
@@ -9,7 +9,7 @@ export const useLogin = () => {
     const login = async (email,password) => {
         setLoading(true)
         setError(null)
-        const response = await fetch('http://localhost:4000/login',{
+        const response = await fetch('https://webstore-mern.herokuapp.com/login',{
             method: 'POST',
             headers: {'Content-Type' : 'application/json'},
             body: JSON.stringify({email,password})
--- frontend/src/components/useSignup.js
+++ frontend/src/components/useSignup.js
@@ -9,7 +9,7 @@ export const useSignup = () => {
     const signup = async (email,password) => {
         setLoading(true)
         setError(null)
-        const response = await fetch('http://localhost:4000/signup',{
+        const response = await fetch('https://webstore-mern.herokuapp.com/signup',{
             method: 'POST',
             headers: {'Content-Type' : 'application/json'},
             body: JSON.stringify({email,password})
